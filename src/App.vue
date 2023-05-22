ce2906af48bad0b902bf0daf41c2d4228e66f40e
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-05-22 15:51:04 +0200

    Start

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+
+import HeaderApp from './components/HeaderApp.vue';
+import CardList from './components/CardList.vue';
+import CardApp from './components/CardApp.vue';
+
+
+export default {
+
+  components: {
+    HeaderApp,
+    CardList,
+    CardApp,
+  }
+}
+</script>
+
+
+<template>
+
+  <HeaderApp/>
+
+  <CardList/>
+
+  <CardApp/>
+  
+</template>
+
+
+<style lang="scss">
+  
+</style>
